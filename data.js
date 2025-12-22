d65f7aa9a6917ce2d0b0e2385b5d22d51ed47457
AmarIsEpic <amarmehadzic@gmail.com>
2025-12-22 19:47:07 +0100

    data.js file addition

---
+++ data.js
@@ -0,0 +1,225 @@
+const albums = [
+    {
+        id: 'kill-em-all',
+        title: "Kill 'em all",
+        cover: 'Images/Killemall.jpg',
+        songs: [
+            'Hit the Lights',
+            'The Four Horsemen',
+            'Motorbreath',
+            'Jump in the Fire',
+            'Whiplash',
+            'Phantom Lord',
+            'No Remorse',
+            'Seek & Destroy',
+            'Metal Militia'
+        ]
+    },
+
+        {
+        id: 'ride-the-lightning',
+        title: "Ride the lightning",
+        cover: 'Images/Ridethelightning.jpg',
+        songs: [
+            'Fight Fire with Fire',
+            'Ride the Lightning',
+            'From Whom the Bell Tolls',
+            'Fade to Black',
+            'Trapped Under Ice',
+            'Escape',
+            'Creeping Death',
+            'The Call of Ktulu',
+        ]
+    },
+
+        {
+        id: 'master-of-puppets',
+        title: "Master of puppets",
+        cover: 'Images/Masterofpuppets.png',
+        songs: [
+            'Battery',
+            'Master of Puppets',
+            'The Thing That Should Not Be',
+            'Welcome Home (Sanitarium)',
+            'Disposable Heroes',
+            'Leper Messiah',
+            'Orion',
+            'Damage, Inc.',
+        ]
+    },
+
+        {
+        id: 'and-justice-for-all',
+        title: "...and justice for all",
+        cover: 'Images/Justiceforall.png',
+        songs: [
+            'Blackened',
+            '...And Justice for All',
+            'Eye of the Beholder',
+            'One',
+            'The Shortest Straw',
+            'Harvester of Sorrow',
+            'The Frayed Ends of Sanity',
+            'To Live Is to Die',
+            'Dyers Eve'
+        ]
+    },
+
+        {
+        id: 'metallica',
+        title: "Metallica (the black album)",
+        cover: 'Images/Blackalbum.jpg',
+        songs: [
+            'Enter Sandman',
+            'Sad but True',
+            'Holier Than Thou',
+            'The Unforgiven',
+            'Wherever I May Roam',
+            "Don't Tread on Me",
+            'Through the Never',
+            'Nothing Else Matters',
+            'Of Wolf and Man',
+            'The God that Failed',
+            'My Friend of Misery',
+            'The Struggle Within'
+        ]
+    },
+
+        {
+        id: 'load',
+        title: "Load",
+        cover: 'Images/Load.png',
+        songs: [
+            "Ain't My Bitch",
+            '2 X 4',
+            'The House Jack Built',
+            'Until It Sleeps',
+            'King Nothing',
+            'Hero of the Day',
+            'Bleeding Me',
+            'Cure',
+            'Poor Twisted Me',
+            'Wasting My Hate',
+            'Mama Said',
+            'Thorn Within',
+            'Ronnie',
+            'The Outlaw Torn'
+        ]
+    },
+
+        {
+        id: 'reload',
+        title: "Reload",
+        cover: 'Images/Reload.png',
+        songs: [
+            'Fuel',
+            'The Memory Remains',
+            "Devil's Dance",
+            'The Unforgien II',
+            'Better Than You',
+            'Slither',
+            'Carpe Diem Baby',
+            'Bad Seed',
+            'Where the Wild Things Are',
+            'Prince Charming',
+            "Low Man's Lyric",
+            'Attitude',
+            'Fixxxer',
+        ]
+    },
+
+        {
+        id: 'st-anger',
+        title: "St. anger",
+        cover: 'Images/Stanger.png',
+        songs: [
+            'Frantic',
+            'St. Anger',
+            'Some Kind of Monster',
+            'Dirty Window',
+            'Invisble Kid',
+            'My World',
+            'Shoot Me Again',
+            'Sweet Amber',
+            'The Unnamed Feeling',
+            'Purify',
+            'All Whithin My Hands'
+        ]
+    },
+
+    {
+        id: 'death-magnetic',
+        title: "Death magnetic",
+        cover: 'Images/Deathmagnetic.jpg',
+        songs: [
+            'That Was Just Your Life',
+            'The End of the Line',
+            'Broken, Beat & Scarred',
+            'The Day That Never Comes',
+            'All Nightmare Long',
+            'Cyanide',
+            'The Unforgiven III',
+            'The Judas Kiss',
+            'Suicide & Redemption',
+            'My Apocalypse'
+        ]
+    },
+
+        {
+        id: 'hardwired-to-self-destruct',
+        title: "Hardwired...to self-destruct",
+        cover: 'Images/Hardwired.png',
+        songs: [
+            'Hardwired',
+            'Atlas, Rise!',
+            "Now That We're Dead",
+            'Moth Into Flame',
+            'Dream No More',
+            'Halo on Fire',
+            'Confusion',
+            'ManUNkind',
+            'Here Comes Revenge',
+            'Am I Savage?',
+            'Murder One',
+            'Spit Out the Bone'
+        ]
+    },
+
+        {
+        id: '72-easons',
+        title: "72 seasons",
+        cover: 'Images/72seasons.png',
+        songs: [
+            '72 Seasons',
+            'Shadows Follow',
+            'Screaming Suicide',
+            'Sleepwalk My Life Away',
+            'You Must Burn!',
+            'Lux Æterna',
+            'Crown of Barbed Wire',
+            'Chasing Light',
+            'If Darkness Had a Son',
+            'Too Far Gone?',
+            'Room of Mirrors',
+            'Inamorata'
+        ]
+    },
+
+        {
+        id: 's-and-m',
+        title: "S&M",
+        cover: 'Images/S&m.png',
+        songs: [
+            'Hit the Lights',
+            'The Four Horsemen',
+            'Motorbreath',
+            'Jump in the Fire',
+            'Whiplash',
+            'Phantom Lord',
+            'No Remorse',
+            'Seek & Destroy',
+            'Metal Militia'
+        ]
+    },
+
+]
